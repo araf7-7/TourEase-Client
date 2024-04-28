35592c56d5bcc28d88019ba8cf3dd267efefde35
arafahmed7 <aaraf772@gmail.com>
2024-04-29 02:22:35 +0600

    added tourist section in home

--- assignment-10-client/src/components/Home.jsx
+++ assignment-10-client/src/components/Home.jsx
@@ -3,6 +3,8 @@ import Banner from "./Banner";
 
 // import ResortCard from "./ResortCard";
 import CountryCard from './CountryCard';
+import { useEffect, useState } from "react";
+import TouristSection from "./TouristSection";
 
 
 
@@ -12,14 +14,25 @@ import CountryCard from './CountryCard';
 
 const Home = () => {
     const categories = useLoaderData();
-    
+    const [places, setPlaces] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/place")
+            .then(res => res.json())
+            .then(data => setPlaces(data));
+    }, [])
 
     return (
         <>
 
             <Banner></Banner>
 
-            
+            <section className="max-w-6xl mx-auto mt-32">
+                <h2 className="text-3xl lg:text-5xl font-bold  text-center mx-auto">Tourist Spots</h2>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10 mx-2">
+                    {places?.slice(1, 7).map(place => <TouristSection key={place.id} place={place}></TouristSection>)}
+                </div>
+            </section>
             
 
             <div className="container mx-auto items-center justify-center mt-20">
---
+++ assignment-10-client/src/components/TouristSection.jsx
@@ -0,0 +1,37 @@
+import { Fade } from "react-awesome-reveal";
+import { Link } from "react-router-dom";
+
+const TouristSection = ({ place }) => {
+
+    const { _id, name, photo, description } = place || {};
+    return (
+        <div>
+            <div
+                className="max-w-full h-full max-h-auto rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800 border-2" >
+                <img
+                    src={photo}
+
+                    className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
+                <div className="flex flex-col justify-between p-6 space-y-2">
+                    <div className="space-y-2">
+                        <Fade duration={3000}>
+                            <h2 className="text-lg font-semibold" >{name}</h2>
+                            <small><p className="dark:text-gray-800" >{ description}</p></small>
+                            <div>
+                                <Link to={`/place/${_id}`}>
+                                    <button className="btn  w-full text-white bg-gradient-to-r bg-yellow-500 hover:bg-yellow-600">View Details</button>
+                                </Link>
+                            </div>
+                        </Fade>
+
+
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TouristSection;
